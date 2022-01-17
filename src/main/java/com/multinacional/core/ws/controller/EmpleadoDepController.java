32801fef27a2bf69d5324a6af19e3b53bba323e6
dangulo <dangulo@cofrico.com>
2022-01-17 16:44:43 +0100

    Commit 2: EmpleadoDep corregido

--- src/main/java/com/multinacional/core/ws/controller/EmpleadoDepController.java
+++ src/main/java/com/multinacional/core/ws/controller/EmpleadoDepController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @Slf4j
+
 @RestController
 @RequestMapping("/empleadosdep")
 public class EmpleadoDepController {
